002e01d05482e9c71b7d59e38663c43c8045b1bb
Candice Marx <candice.marx@hotmail.com>
2019-08-02 08:12:24 -0600

    began react star game

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import StarMatch from './StarMatch';
+
+function App() {
+  return (
+    < StarMatch />
+  );
+}
+
+export default App;
